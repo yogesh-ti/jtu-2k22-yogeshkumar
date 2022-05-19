fedee39b8f696b83fc2e863681b1b1ae60132542
Yogesh Kumar <yogesh.kumar@trilogy.com>
2022-05-19 22:17:10 +0000

    Update: Removed unused imports for numpy and pandas

--- restapi/views.py
+++ restapi/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from decimal import Decimal
-import pandas as pd
-import numpy as np
 import urllib.request
 from datetime import datetime
 
